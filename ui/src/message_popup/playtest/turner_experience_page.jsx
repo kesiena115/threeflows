0637c358be9b988458b85819921c84cc8b0b1d2b
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2017-06-01 17:55:02 -0400

    Add background context and reflection questions to Jennifer Turner Simulation

--- ui/src/message_popup/playtest/turner_experience_page.jsx
+++ ui/src/message_popup/playtest/turner_experience_page.jsx
@@ -1,8 +1,6 @@
 /* @flow weak */
-import _ from 'lodash';
 import React from 'react';
 import uuid from 'uuid';
-import VelocityTransitionGroup from "velocity-react/velocity-transition-group";
 
 
 import * as Api from '../../helpers/api.js';
@@ -19,10 +17,6 @@ type ResponseT = {
   choice:string,
   question:QuestionT
 };
-
-
-
-
 
 // The top-level page, manages logistics around email, questions,
 // and the display of instructions, questions, and summary.
@@ -78,7 +72,8 @@ export default React.createClass({
     if (responses.length >= questions.length) {
       return null;
     }
-    return questions[responses.length];
+    const question = questions[responses.length];
+    return {id: question.id, question:question, responses:responses};
   },
 
   render() {
@@ -107,107 +102,110 @@ export default React.createClass({
       <IntroWithEmail defaultEmail={this.state.email} onDone={this.onStart}>
         <VideRecorder />
         <div>
-            <p>Welcome!</p>
-            <p>You will go through a set of video scenarios that simulate the conversation between you and a parent.</p>
-            <p>You need to provide an audio response to each prompt. Please respond as quickly as possible (like you would do in a real conversation).</p>
-            <p>This may feel uncomfortable at first, but it's better to feel uncomfortable here than with a real parent.</p>
+          <p>Welcome!</p>
+          <p>You will go through a couple of activities that are designed to make you better prepared for a potentially difficult parent-teacher conference you might experience as a new teacher.</p>
+          <p>You need to use a computer/laptop that has a mic because you will need to do audio recordings.</p>
+          <p>This simulation is based on work that has been done by Professor Benjamin Dotger at Syracuse University as documented in his book: "I Had No Idea" Clinical Simulations for Teacher Development.</p>
+          <p>This activity would take about 30minutes.</p>
         </div>
       </IntroWithEmail>
     );
   },
 
-  renderQuestionEl(question:QuestionT, onLog, onResponseSubmitted) {
+  renderQuestionEl(nextQuestion:NextQuestionT, onLog, onResponseSubmitted) {
+    const {question, responses} = nextQuestion;
+
+    if (question.stage === 'scenario') {
+      return (
+        <InstantResponseScenario 
+          onLogMessage={onLog}
+          onResponseSubmitted={this.onRecordingDone.bind(this, onResponseSubmitted)}
+          question={question}
+        />
+      );
+    }
+
+    if (question.stage === 'prereflect') {
+      return (
+        <div key={JSON.stringify(question)}>
+          <MixedQuestion question={question} />
+          <MinimalTextResponse
+            forceResponse={true}
+            responsePrompt="Your Answer"
+            recordText="NEXT"
+            onLogMessage={onLog}
+            onResponseSubmitted={onResponseSubmitted}
+          />
+        </div>
+      );
+    }
+
+    if (question.stage === 'postreflect') {
+      return (
+        <div key={JSON.stringify(question)}>
+          <MixedQuestion question={question} />
+          <MinimalTextResponse
+            forceResponse={true}
+            responsePrompt="Your Answer"
+            recordText="NEXT"
+            onLogMessage={onLog}
+            onResponseSubmitted={onResponseSubmitted}
+          />
+          {this.renderScenarioResponsesEl(this.state.questions, responses)}
+        </div>
+      );
+    }
+
+    // info
     return (
-      <InstantResponseScenario 
-        onLogMessage={onLog}
-        onResponseSubmitted={this.onRecordingDone.bind(this, onResponseSubmitted)}
-        question={question}
-      />
-    ); 
+      <div key={JSON.stringify(question)}>
+        <MixedQuestion question={question} />
+        <ChoiceForBehaviorResponse
+          choices={['NEXT']}
+          onLogMessage={onLog}
+          onResponseSubmitted={onResponseSubmitted}
+        />
+      </div>
+    );
+
   },
 
   renderSummaryEl(questions:[QuestionT], responses:[ResponseT]) {
     return (
-      <div>
-        <VelocityTransitionGroup enter={{animation: "slideDown"}} leave={{animation: "slideUp"}} runOnMount={true}>
-          <div style={styles.doneTitle}>
-            <p style={styles.paragraph}>You've finished the simulation.</p>
-            <p style={styles.paragraph}><strong>Do not close this page</strong>. You will need it for the next reflection.</p>
-            <p style={styles.paragraph}>Please return to the form for the post-simulation reflection.</p>
-          </div>
-          <p style={styles.summaryTitle}>Summary</p>
-
-          {responses.map((response, i) => 
-            <div key={"question-" + i} style ={_.merge(styles.instructions, styles.summaryQuestion)}>
-              <div style={styles.videoContainer}>
-                <YouTube
-                  videoId={questions[i].youTubeId}
-                  opts={{
-                    height: '100%',
-                    width: '100%',
-                    playerVars: { // https://developers.google.com/youtube/player_parameters
-                      autoplay: 0,
-                      controls: 1,
-                      rel: 0,
-                      showinfo: 0,
-                      start: questions[i].start,
-                      end: questions[i].end
-                    }
-                  }} 
-                />
-              </div>
-              <p style={styles.responseLabel}>Your Response:</p>
-              <audio key={'response-' + i} controls={true} src={response.downloadUrl} style={{paddingTop: 0, paddingBottom: 20}}/>
-            </div>
-          
-          )}
-          <div style={styles.container} />
-        </VelocityTransitionGroup>
+      
+      <div className="done">
+        <b style={{
+          display: 'block',
+          padding: '15px 20px 15px',
+          background: '#09407d',
+          color: 'white'
+        }}>Done</b>
+        <div style={styles.doneTitle}>
+          <p style={styles.paragraph}>You have now completed the simulation and personal reflections</p>
+          <p style={styles.paragraph}><a href={Routes.Home}>Back to Home</a></p>
+        </div>
       </div> 
+    );
+  },
+
+  renderScenarioResponsesEl(questions:[QuestionT], responses:[ResponseT]) {
+    return (
+      <VideoSummary
+        questions={questions}
+        responses={responses}
+        delayRenderingMs={500} /> 
     );
   }
 });
 
 const styles = {
-  container: {
-    fontSize: 20,
-    padding: 0,
-    margin:0,
-    paddingBottom: 45
-  },
   doneTitle: {
     padding: 20,
     paddingBottom: 0,
     margin:0,
   },
-  instructions: {
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
   paragraph: {
     marginTop: 20,
     marginBottom: 20
-  },
-  summaryTitle: {
-    fontSize: 20,
-    padding: 20,
-    paddingBottom: 5,
-    margin: 0,
-    fontWeight: 'bold'
-  },
-  summaryQuestion: {
-    whiteSpace: 'pre-wrap',
-    fontSize: 16,
-    lineHeight: 1.2,
-    paddingTop: 10,
-    paddingBottom: 10
-  },
-  responseLabel: {
-    marginTop: 10,
-    marginBottom: 0,
-    fontSize: 12
-  },
-  videoContainer: {
-    height: 230
   }
 };
